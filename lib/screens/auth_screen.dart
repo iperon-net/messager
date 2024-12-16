71dc8008e039466e56f9c5a63ea8505999b30aa0
Kostya Ten <kostya@yandex.ru>
2024-12-16 23:48:07 +0300

    fix: Delete unused import

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -2,7 +2,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 import 'package:messenger/cubit/debug_cubit.dart';
 
 class AuthScreen extends StatelessWidget {
