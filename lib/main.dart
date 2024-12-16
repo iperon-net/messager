9ec52accad10c6571de509222b5703a3938528b1
Kostya Ten <kostya@yandex.ru>
2024-12-16 18:57:49 +0300

    fix: small

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:messenger/db/db.dart';
-import 'package:messenger/db/models.dart' as modeldb;
 import 'package:messenger/utils.dart';
 
 import 'cubit/counter_cubit.dart';
