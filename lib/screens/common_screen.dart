f15b6c411688aa9d7b2546b5ce9d43c0a52ff828
Kostya Ten <kostya@yandex.ru>
2024-12-18 01:50:08 +0300

    fix: Added isDebug mode

--- lib/screens/common_screen.dart
+++ lib/screens/common_screen.dart
@@ -2,12 +2,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import '../injection.dart';
+import '../utils.dart';
+
 class CommonScreen {
+  Utils utils = getIt.get<Utils>();
 
   Widget notImplemented() {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+    return MaterialApp(
+      debugShowCheckedModeBanner: utils.isDebug ? true : false,
+      home: const Scaffold(
         body: Center(
           child: Text("Not implemented"),
         ),
