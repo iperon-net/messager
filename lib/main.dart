0c1db48b86f89724cf5cad29d94b4435c9ad2a38
Kostya Ten <kostya@yandex.ru>
2024-12-18 11:26:58 +0300

    fix: Delete unused import

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:messenger/cubit/auth_cubit.dart';
 import 'package:messenger/utils.dart';
 
 import 'constants.dart';
